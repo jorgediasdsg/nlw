04ba417bb697b108fc49abe5c6bca1abea747442
Jorge Dias <jorgediasdsg@gmail.com>
2020-06-12 22:33:47 -0300

    Up

--- server/src/router.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const routes = express.Router();
-
-app.get('/', (request, response) =>{
-    return response.json({ message: 'Hello Word'});
-});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';  
+import routes from './routes';
 
 const app = express();  
 
 app.use(express.json()); //Importando json do express
 
-app.get('/',(request, response) => {
-    return response.json({ message: 'Hello world!'}); // retornando dados.
-});
+app.use(routes);
 
 app.listen(3333);
