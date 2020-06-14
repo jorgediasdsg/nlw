006d3604f28fd5395fbea6636eba25dd6b708a7f
Jorge Dias <jorgediasdsg@gmail.com>
2020-06-14 17:06:18 -0300

    Apos limpeza estrutura

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
 
+// Como tudo funciona o react?
+// index.html tem uma DIV root
+// A DIV root está apontada no arquivo index.tsx
+// No arquivo index.TSX tem uma tag em react chamada <App> com a importação deste arquivo app.tsx
+// Aqui embaixo nessa DIV colocamos a função App retornando html puro e ...
+// Voalá
+
+//JSX 
+
 function App() {
   return (
-        <h1>Rodandooo!</h1>
+        <h1>Rodandooo!!</h1>
   );
 }
 
