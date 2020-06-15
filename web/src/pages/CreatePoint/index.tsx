d95983c8a909a626b1bf450b04415ce5f532e6f0
Jorge Dias <jorgediasdsg@gmail.com>
2020-06-14 22:16:58 -0300

    Rotas Finalizada

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import axios from 'axios';
 import api from '../../services/api';
 
 import './styles.css';
@@ -16,9 +17,14 @@ interface Item {
     image_url: string
 }
 
-const CreatePoint = () => {
-    const [itens, setItens] = useState<Item[]>([]);
+interface IBGEUFResponse {
+    sigla: string;
+}
 
+const CreatePoint = () => {
+
+    const [itens, setItens] = useState<Item[]>([]);
+    const [ufs, setUfs] = useState<string[]>([]);
 
     useEffect(() => {
         api.get('itens').then(response =>{
@@ -26,7 +32,22 @@ const CreatePoint = () => {
         })
     }, [] );
 
+    useEffect(() => {
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+        .then(response =>{
+            const ufInitials = response.data.map(uf => uf.sigla);
 
+            setUfs(ufInitials);
+        })
+    });
+
+    useEffect(() => {
+
+    });
+
+
+
+    
 
     return (
         <div id="page-create-point">
@@ -89,6 +110,9 @@ const CreatePoint = () => {
                             <label htmlFor="uf">Estado UF</label>
                             <select name="uf" id="uf">
                                 <option value="0">Selecione uma UF</option>
+                                {ufs.map(uf => (
+                                    <option key={uf} value="{uf}">{uf}</option>
+                                ))};
                             </select>
                         </div>
                         <div className="field">
