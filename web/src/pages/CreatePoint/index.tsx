02a608a6445c77ac57f4ca5dea14fec9d8b2ca97
Jorge Dias <jorgediasdsg@gmail.com>
2020-06-15 00:02:55 -0300

    Encerrado aula03

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState, ChangeEvent, FormEvent} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
@@ -51,6 +51,7 @@ const CreatePoint = () => {
     //         setinitialPosition([latitude, longitude]);
     //     });
     // }, []);
+    const history = useHistory();
 
     useEffect(() => {
         api.get('itens').then(response =>{
@@ -137,6 +138,8 @@ const CreatePoint = () => {
         await api.post('points', data);
 
         alert('Ponto de coleta criado.')
+
+        history.push('/');
     }
 
     return (
